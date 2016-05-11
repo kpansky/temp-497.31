e7ba12ff0dc71d32aad01e97d205bcaa2e8810c8
Kevin Pansky <kevin.pansky@gmail.com>
2016-05-10 22:50:17 -0500

    Fix stuff and things

--- src/dtmf_detect_task.c
+++ src/dtmf_detect_task.c
@@ -1,3 +1,4 @@
+#include <math.h>
 
 /* FreeRTOS.org includes. */
 #include "FreeRTOS.h"
@@ -9,23 +10,80 @@
 
 #include "dtmf_detect_task.h"
 #include "dtmf_data.h"
+#include "fft/fft.h"
 
 static struct DTMFSamples_t s;
+static struct DTMFResult_t r;
+complex cs[DTMFSampleSize];
+
+#define spec_power(x) (sqrt((x.Re * x.Re) + (x.Im * x.Im)))
+void pick_peaks(complex *cs, float thresh, int16_t *toneA, int16_t *toneB);
+int16_t decode_tones(int16_t toneA, int16_t toneB);
 
 void vDTMFDetectTask( void *pvParameters ) {
 
 	struct DTMFDetectTaskParam_t* params = (struct DTMFDetectTaskParam_t *)pvParameters;
 
 	vPrintString( "DTMF Detector started\n" );
+	init_Wn();
 
 	for( ;; )
 	{
 		xQueueReceive( params->sampQ, &s, portMAX_DELAY );
 
-		/* Do real work here */
-		struct DTMFResult_t r;
 		vPrintString( "DTMF Detector working\n" );
+
+		int ii;
+		for (ii=0; ii<DTMFSampleSize; ii++) {
+			cs[ii].Re = (float)s.samp[ii] / 16384.0f; // Need to convert this to float
+			cs[ii].Im = 0.0f;
+		}
+
+		/* Do real work here */
+		fft(cs, DTMFSampleSize); // Need to ensure second arg is log2(DTMFSampleSize)
+		pick_peaks(cs, 5.0f, &r.toneA, &r.toneB);
+		r.code = decode_tones(r.toneA,r.toneB);
 
 		xQueueSendToBack( params->resultQ, &r, portMAX_DELAY );
 	}
+}
+
+void pick_peaks(complex *cs, float threshold, int16_t *toneA, int16_t *toneB) {
+
+	*toneA = -1;
+	*toneB = -1;
+
+	/* check low bins for power */
+	if (spec_power(cs[DTMF_L0_BIN]) >= threshold) {
+		*toneA = DTMF_L0_FREQ;
+		vPrintString( "Detected tone L0\n" );
+	} else if (spec_power(cs[DTMF_L1_BIN]) >= threshold) {
+		*toneA = DTMF_L1_FREQ;
+		vPrintString( "Detected tone L1\n" );
+	} else if (spec_power(cs[DTMF_L2_BIN]) >= threshold) {
+		*toneA = DTMF_L2_FREQ;
+		vPrintString( "Detected tone L2\n" );
+	} else if (spec_power(cs[DTMF_L3_BIN]) >= threshold) {
+		*toneA = DTMF_L3_FREQ;
+		vPrintString( "Detected tone L3\n" );
+	}
+
+	/* check high bins for power */
+	if (spec_power(cs[DTMF_H0_BIN]) >= threshold) {
+		*toneB = DTMF_H0_FREQ;
+		vPrintString( "Detected tone H0\n" );
+	} else if (spec_power(cs[DTMF_H1_BIN]) >= threshold) {
+		*toneB = DTMF_H1_FREQ;
+		vPrintString( "Detected tone H1\n" );
+	} else if (spec_power(cs[DTMF_H2_BIN]) >= threshold) {
+		*toneB = DTMF_H2_FREQ;
+		vPrintString( "Detected tone H2\n" );
+	} else if (spec_power(cs[DTMF_H3_BIN]) >= threshold) {
+		*toneB = DTMF_H3_FREQ;
+		vPrintString( "Detected tone H3\n" );
+	}
+}
+
+int16_t decode_tones(int16_t toneA, int16_t toneB) {
+	return -1;
 }
--- src/testbench_task.c
+++ src/testbench_task.c
@@ -1,4 +1,5 @@
-#include "string.h"
+#include <string.h>
+#include <math.h>
 
 /* FreeRTOS.org includes. */
 #include "FreeRTOS.h"
@@ -21,43 +22,43 @@ struct TB_Tone_t {
 
 static struct TB_Tone_t tones[] =
 	{
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L0_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L1_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L2_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L3_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_H0_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_H1_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_H2_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_H3_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L0_PERIOD,	DTMF_H0_PERIOD},
-		{	DTMF_L0_PERIOD,	DTMF_H1_PERIOD},
-		{	DTMF_L0_PERIOD,	DTMF_H2_PERIOD},
-		{	DTMF_L0_PERIOD,	DTMF_H3_PERIOD},
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L1_PERIOD,	DTMF_H0_PERIOD},
-		{	DTMF_L1_PERIOD,	DTMF_H1_PERIOD},
-		{	DTMF_L1_PERIOD,	DTMF_H2_PERIOD},
-		{	DTMF_L1_PERIOD,	DTMF_H3_PERIOD},
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L2_PERIOD,	DTMF_H0_PERIOD},
-		{	DTMF_L2_PERIOD,	DTMF_H1_PERIOD},
-		{	DTMF_L2_PERIOD,	DTMF_H2_PERIOD},
-		{	DTMF_L2_PERIOD,	DTMF_H3_PERIOD},
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
-		{	DTMF_L3_PERIOD,	DTMF_H0_PERIOD},
-		{	DTMF_L3_PERIOD,	DTMF_H1_PERIOD},
-		{	DTMF_L3_PERIOD,	DTMF_H2_PERIOD},
-		{	DTMF_L3_PERIOD,	DTMF_H3_PERIOD},
-		{	DTMF_NO_PERIOD,	DTMF_NO_PERIOD},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L0_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L1_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L2_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L3_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_H0_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_H1_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_H2_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_H3_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L0_FREQ,	DTMF_H0_FREQ	},
+		{	DTMF_L0_FREQ,	DTMF_H1_FREQ	},
+		{	DTMF_L0_FREQ,	DTMF_H2_FREQ	},
+		{	DTMF_L0_FREQ,	DTMF_H3_FREQ	},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L1_FREQ,	DTMF_H0_FREQ	},
+		{	DTMF_L1_FREQ,	DTMF_H1_FREQ	},
+		{	DTMF_L1_FREQ,	DTMF_H2_FREQ	},
+		{	DTMF_L1_FREQ,	DTMF_H3_FREQ	},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L2_FREQ,	DTMF_H0_FREQ	},
+		{	DTMF_L2_FREQ,	DTMF_H1_FREQ	},
+		{	DTMF_L2_FREQ,	DTMF_H2_FREQ	},
+		{	DTMF_L2_FREQ,	DTMF_H3_FREQ	},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
+		{	DTMF_L3_FREQ,	DTMF_H0_FREQ	},
+		{	DTMF_L3_FREQ,	DTMF_H1_FREQ	},
+		{	DTMF_L3_FREQ,	DTMF_H2_FREQ	},
+		{	DTMF_L3_FREQ,	DTMF_H3_FREQ	},
+		{	DTMF_NO_FREQ,	DTMF_NO_FREQ	},
 	};
 static int num_tones = sizeof(tones)/sizeof(tones[0]);
+float pi = 3.14159265359f;
 
-void generate_tone(uint16_t period, struct DTMFSamples_t *s);
+void generate_tone(float freq, struct DTMFSamples_t *s);
 void print_results(struct DTMFResult_t *r);
-
 
 void vTestBenchTask( void *pvParameters ) {
 	int tone_index = 0;
@@ -76,116 +77,25 @@ void vTestBenchTask( void *pvParameters ) {
 		if (tone_index == num_tones) tone_index = 0;
 
 		/* Pass the synthetic data to the detector */
-		vPrintString( "Testbench sent data\n" );
+		//vPrintString( "Testbench sent data\n" );
 		xQueueSendToBack( params->sampQ, &samps, portMAX_DELAY );
 
 		xQueueReceive( params->resultQ, &result, portMAX_DELAY );
-		vPrintString( "Testbench received results\n" );
+		//vPrintString( "Testbench received results\n" );
 
 		print_results(&result);
 	}
 }
 
-void generate_tone(uint16_t p, struct DTMFSamples_t *s) {
-	static DTMFSampleType amp = 500;
+void generate_tone(float freq, struct DTMFSamples_t *s) {
 	int ii;
-
-	if (p != DTMF_NO_PERIOD) {
-		for (ii=0; ii<DTMFSampleSize; ii++) {
-//#define __CORDIC__
-#ifndef __CORDIC__
-			if (((ii / (p/2)) % 2) == 0) {
-				s->samp[ii] += amp;
-			}
-			else {
-				s->samp[ii] -= amp;
-			}
-#else
-			float fx = (float)amp;
-			float fy = 0.0f;
-			float fz = need to calculat phasor rate
-			cordic (fx,fy,fz,15);
-			s->samp[ii] = (DTMFSampleType)fx;
-#endif
-		}
+	float amp = 4000.0f;
+	float fz_tmp = 2.0f * pi * freq / (float)DTMFSampleRate;
+	for (ii=0; ii<DTMFSampleSize; ii++) {
+		s->samp[ii] += (amp * sin(fz_tmp * (float)ii));
 	}
 }
 
 void print_results(struct DTMFResult_t *r) {
 
-}
-
-
-
-
-float pi = 3.14159265359f;
-
-float angles[] = {
-    0.78539816339745f,	0.46364760900081f,	0.24497866312686f,	0.12435499454676f,
-    0.06241880999596f,	0.03123983343027f,	0.01562372862048f,	0.00781234106010f,
-    0.00390623013197f,	0.00195312251648f,	0.00097656218956f,	0.00048828121119f,
-    0.00024414062015f,	0.00012207031189f,	0.00006103515617f,	0.00003051757812f,
-    0.00001525878906f,	0.00000762939453f,	0.00000381469727f,	0.00000190734863f,
-    0.00000095367432f,	0.00000047683716f,	0.00000023841858f,	0.00000011920929f,
-    0.00000005960464f,	0.00000002980232f,	0.00000001490116f,	0.00000000745058f,
-	};
-int angles_n = sizeof(angles)/sizeof(angles[0]);
-
-float Kvalues[] = {
-    0.70710678118655f,	0.63245553203368f,	0.61357199107790f,	0.60883391251775f,
-    0.60764825625617f,	0.60735177014130f,	0.60727764409353f,	0.60725911229889f,
-    0.60725447933256f,	0.60725332108988f,	0.60725303152913f,	0.60725295913894f,
-    0.60725294104140f,	0.60725293651701f,	0.60725293538591f,	0.60725293510314f,
-    0.60725293503245f,	0.60725293501477f,	0.60725293501035f,	0.60725293500925f,
-    0.60725293500897f,	0.60725293500890f,	0.60725293500889f,	0.60725293500888f,
-	};
-int Kvalues_n = sizeof(Kvalues)/sizeof(Kvalues[0]);
-
-void cordic(float *x, float *y, float *z, int n) {
-
-	float angle;
-	float sigma;
-	float flip = 1.0f;
-
-	if (n > Kvalues_n) {
-		n = Kvalues_n;
-	}
-	if (n > angles_n) {
-		n = angles_n;
-	}
-
-	if ((*z < (-pi/2)) || (*z > (pi/2))) {
-		if (z < 0) {
-			*z += pi;
-		} else {
-			*z -= pi;
-		}
-		flip = -1.0f;
-	}
-
-	float Kn = Kvalues[n];
-	float poweroftwo = 1.0f;
-
-	int j;
-	for (j = 0; j<n; j++) {
-
-		if (z < 0) {
-			sigma = -1.0f;
-		} else {
-			sigma = 1.0f;
-		}
-
-		angle = angles[j];
-
-		float factor = sigma * poweroftwo;
-		float x_new = *x - factor * *y;
-		float y_new = *y + factor * *x;
-		*x = x_new;
-		*y = y_new;
-		*z = *z - sigma * angle;
-		poweroftwo /= 2.0f;
-	}
-
-	*x *= flip;
-	*y *= flip;
 }
